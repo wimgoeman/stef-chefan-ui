6d0a9152827e4b3c97aea8b4a659eea0233f2795
Wim Goeman <wimgoeman@gmail.com>
2019-10-16 22:20:59 +0200

    Change environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,6 @@ export const environment = {
   production: true,
   apiUrl: "https://stef-chefan.appspot.com",
   firebase: {
-    apiKey: "AIzaSyAoJYHZCuX_h006vrt1TrkaTcnuZ_4CJK0",
-    authDomain: "stef-chefan.firebaseapp.com",
-    databaseURL: "https://stef-chefan.firebaseio.com",
-    projectId: "stef-chefan",
-    storageBucket: "stef-chefan.appspot.com",
-    messagingSenderId: "141241829645",
-    appId: "1:141241829645:web:8cc21bafa4698339878844"
+    // Put firebase data here
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,7 @@ export const environment = {
   production: false,
   apiUrl: "http://localhost:8080",
   firebase: {
-    apiKey: "AIzaSyAoJYHZCuX_h006vrt1TrkaTcnuZ_4CJK0",
-    authDomain: "stef-chefan.firebaseapp.com",
-    databaseURL: "https://stef-chefan.firebaseio.com",
-    projectId: "stef-chefan",
-    storageBucket: "stef-chefan.appspot.com",
-    messagingSenderId: "141241829645",
-    appId: "1:141241829645:web:8cc21bafa4698339878844"
+    // Put firebase data here
   }
 };
 
