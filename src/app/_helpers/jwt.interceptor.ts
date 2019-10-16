4bca2a04fbf15a2758d2b71da0c8b6f7c7efe174
Wim Goeman <wimgoeman@gmail.com>
2019-10-16 20:41:38 +0200

    Restore authService on JwtInterceptor

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor() { }
+
+    constructor(private authService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("intercepting... " + request.url)
