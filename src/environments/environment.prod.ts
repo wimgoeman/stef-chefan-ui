380a597328c5bec50442c8ec6ba33b91164dca04
Wim Goeman <wimgoeman@gmail.com>
2019-10-16 20:55:53 +0200

    Add env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://localhost:8080",
+  firebase: {
+    apiKey: "AIzaSyAoJYHZCuX_h006vrt1TrkaTcnuZ_4CJK0",
+    authDomain: "stef-chefan.firebaseapp.com",
+    databaseURL: "https://stef-chefan.firebaseio.com",
+    projectId: "stef-chefan",
+    storageBucket: "stef-chefan.appspot.com",
+    messagingSenderId: "141241829645",
+    appId: "1:141241829645:web:8cc21bafa4698339878844"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,15 @@
 
 export const environment = {
   production: false,
+  apiUrl: "http://localhost:8080",
   firebase: {
-    // Paste firebase config here...
+    apiKey: "AIzaSyAoJYHZCuX_h006vrt1TrkaTcnuZ_4CJK0",
+    authDomain: "stef-chefan.firebaseapp.com",
+    databaseURL: "https://stef-chefan.firebaseio.com",
+    projectId: "stef-chefan",
+    storageBucket: "stef-chefan.appspot.com",
+    messagingSenderId: "141241829645",
+    appId: "1:141241829645:web:8cc21bafa4698339878844"
   }
 };
 
